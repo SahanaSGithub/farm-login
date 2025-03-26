c77b40ebdcfda72469f8b3bae13ce1658f9cde37
SahanaSGithub <153353761+SahanaSGithub@users.noreply.github.com>
2025-03-26 12:27:41 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTv544-V2RD1oNocK1sL55A550bQbGVgw",
+  authDomain: "farm-2f0da.firebaseapp.com",
+  projectId: "farm-2f0da",
+  storageBucket: "farm-2f0da.firebasestorage.app",
+  messagingSenderId: "105050441569",
+  appId: "1:105050441569:web:e2f01384d58c77aa27e720",
+  measurementId: "G-5BTQB9C6GC"
+};
