db1d21ea4683af3ac57db29c382e90df158c9b6a
SahanaSGithub <153353761+SahanaSGithub@users.noreply.github.com>
2025-03-26 12:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const auth = firebase.auth();
-
-// Signup Function
+// Sign Up Function
 function signUp() {
     let email = document.getElementById("signup-email").value;
     let password = document.getElementById("signup-password").value;
@@ -8,6 +6,8 @@ function signUp() {
     auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
             alert("Sign Up Successful!");
+            document.getElementById("user-info").innerText = "Signed up as: " + userCredential.user.email;
+            showLogoutButton();
         })
         .catch((error) => {
             alert(error.message);
@@ -22,6 +22,7 @@ function logIn() {
     auth.signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             document.getElementById("user-info").innerText = "Logged in as: " + userCredential.user.email;
+            showLogoutButton();
         })
         .catch((error) => {
             alert(error.message);
@@ -32,7 +33,15 @@ function logIn() {
 function logOut() {
     auth.signOut().then(() => {
         document.getElementById("user-info").innerText = "Logged out";
+        document.getElementById("logout-btn").style.display = "none";
     }).catch((error) => {
         alert(error.message);
     });
 }
+
+// Show Logout Button After Login/Signup
+function showLogoutButton() {
+    document.getElementById("logout-btn").style.display = "block";
+}
+
+
